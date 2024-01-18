d56002037e2c7190cbbab5a01d5d640acab37d63
SN2006 <slavanedelev@gmail.com>
2024-01-18 09:23:45 +0200

    Add app description

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My git app for users.");
+        System.out.println("The informative application.");
     }
 }
