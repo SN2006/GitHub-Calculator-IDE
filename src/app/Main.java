324b56e866504ca68bc984d61e26bd09ae53340f
SN2006 <slavanedelev@gmail.com>
2024-01-20 11:49:59 +0200

    Add multiplication

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,10 +8,16 @@ public class Main {
         int a = 10;
         int b = 4;
         int c = add(a, b);
-        System.out.format("%d + %d = %d", a, b, c);
+        System.out.format("%d + %d = %d\n", a, b, c);
+        int d = multiply(a, b);
+        System.out.format("%d * %d = %d", a, b, d);
     }
 
     private static int add(int a, int b){
         return a + b;
+    }
+
+    private static int multiply(int a, int b){
+        return a * b;
     }
 }
