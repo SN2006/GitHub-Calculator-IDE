6c5e61fd2006e85e81e8b65b9aad53cadc4fa2a2
SN2006 <slavanedelev@gmail.com>
2024-01-20 11:58:28 +0200

    Change app info

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,7 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app for users.");
+        System.out.println("My app for users.");
         System.out.println("The informative application.");
         int a = 10;
         int b = 4;
